f3a98c02cc325778607f9f79b615b5cc9c0b81aa
elsayed <elsayed_nofal@ymail.com>
2017-12-03 17:09:15 +0200

    is_exist

--- src/EasyCrudRouteServiceProvider.php
+++ src/EasyCrudRouteServiceProvider.php
@@ -16,9 +16,10 @@ class EasyCrudRouteServiceProvider extends ServiceProvider
         
         //enject router 
         if(floatval(Application::VERSION) >= 5.3){
-            
+            if(file_exists(base_path().'/routes/easy-crud-route.php'))
             $this->loadRoutesFrom(base_path().'/routes/easy-crud-route.php');
         }else{
+			if(file_exists(app_path().'/Http/easy-crud-route.php'))
             include app_path().'/Http/easy-crud-route.php';
         }
         
